aed630745400b9322386f292149fbd94b4d09afe
Niklas Dzösch <nd@shopware.com>
2016-09-09 09:54:44 +0200

    Added optional debug config to build-unit

--- src/Commands/BuildUnitCommand.php
+++ src/Commands/BuildUnitCommand.php
@@ -5,6 +5,7 @@ namespace ShopwareDockerControl\Commands;
 use ShopwareDockerControl\Services\DockerComposeService;
 use ShopwareDockerControl\Services\ShellCommandRunnerService;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -37,6 +38,13 @@ class BuildUnitCommand extends Command
                 InputOption::VALUE_NONE,
                 'If set, no githool symlink will be created.'
             )
+            ->addOption(
+                'basePath',
+                'bp',
+                InputOption::VALUE_OPTIONAL,
+                'Used for adding debug options to the shopware config.',
+                getenv('PROJECT_BASE_DIR')
+            )
         ;
     }
 
@@ -57,6 +65,10 @@ class BuildUnitCommand extends Command
         $composeService = new DockerComposeService($input, $output);
         $composeService->execute($command);
 
+        $shopwareConfig = $input->getOption('basePath').DIRECTORY_SEPARATOR.$project.DIRECTORY_SEPARATOR.'config.php';
+        
+        $this->createDebugConfig($shopwareConfig);
+
         if ($input->getOption('noSymlink') || strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
             return;
         }
@@ -73,5 +85,27 @@ class BuildUnitCommand extends Command
         $command[] = '/var/www/html/'.$project.'/build/gitHooks/pre-commit';
         $command[] = '/var/www/html/'.$project.'/.git/hooks/pre-commit"';
         $composeService->execute($command);
+    }
+
+    private function createDebugConfig($shopwareConfig)
+    {
+        if (empty(getenv('SWDC_DEBUG_CONFIG_PATH'))) {
+            return;
+        }
+
+        $debugConfigTpl = require getenv('SWDC_DEBUG_CONFIG_PATH');
+        if(!is_array($debugConfigTpl)){
+            throw new InvalidArgumentException("SWDC_DEBUG_CONFIG_PATH does not point to a php file returning an array.");
+        }
+
+        $config = require $shopwareConfig;
+        if(!is_array($config)){
+            throw new \Exception("Could not load shopware config for adding new debug options.");
+        }
+
+        $debugConfig = array_merge($config, $debugConfigTpl);
+        $debugConfig = "<?php\n\nreturn ".var_export($debugConfig, true).';';
+        
+        file_put_contents($shopwareConfig, $debugConfig);
     }
 }
