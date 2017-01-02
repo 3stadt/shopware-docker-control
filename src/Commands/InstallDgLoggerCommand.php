52bb85b5f9f1ee728c449d06ecd8bc7b17bd7bd8
Niklas Dzösch <nd@shopware.com>
2017-01-02 09:58:43 +0100

    Removed unused import

--- src/Commands/InstallDgLoggerCommand.php
+++ src/Commands/InstallDgLoggerCommand.php
@@ -2,7 +2,6 @@
 
 namespace ShopwareDockerControl\Commands;
 
-use Phar;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
